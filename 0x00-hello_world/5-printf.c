4aac5171257e853b7054d3a214d664460cdad57a
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-03 16:08:16 -0700

    today

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,19 +3,14 @@
 
 
 /**
- *
- * * main - prints the size of various types
- *
+ * main -> prints the size of various types
  * * Return: 0 if exited properly, non-zero otherwise
- *
- * */
+  */
 description
 int main(void)
-
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 
 }
