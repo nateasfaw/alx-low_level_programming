2a7c2b9e6490b3ca6026cce87970da00a42d9341
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-03 15:31:33 -0700

    today

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+comment or description
+#include <stdio.h> /
+* main - prints some text
+ *
+ *  *
+ *
+ *   * Return: always 0(success)
+ *
+ *    
+	 */
+
+another description
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
