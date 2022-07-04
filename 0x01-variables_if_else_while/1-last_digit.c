9eb0bfd30d2845edc68712f2cc0ae3366c330a0f
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-04 14:33:33 -0700

    season finale

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
@@ -25,11 +23,11 @@ if (x > 5)
 printf("and is greater than 5");
 }
 printf("and is 0");
-}
-if (x < 6 && x != 0)
 {
-printf("and is less than 6 and not 0");
+if (x < 6 && x != 0)
 }
+printf("and is less than 6 and not 0");
+{
 printf("\n");
 return (0);
 }
