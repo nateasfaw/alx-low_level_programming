91c4575fd321dde2fd83b510211c17ce8f537a6f
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-04 13:46:28 -0700

    slowly amgetting there

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/** 
-*
-*main -> assign a random number to the variable n each time it is executed
+/**
+**main -> assign a random number to the variable n each time it is executed
 *
 **print the last digit of the number stored in the variable n
-**Return: Always 0 (Success)
 *
+**Return: Always 0 (Success)
 */
 
 int main(void)
