3bf72e031eb493308c4693d7a9739fe940622b4e
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-03 17:26:57 -0700

    clow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/*
-*	main -> print the size of various types
-*	return:0 if it exits properly,non zero otherwise
-*/ 
+/**
+* main-> prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle")
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
