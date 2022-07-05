fe3c4d3d4b63d8bd6fbfa3683848e085ef59d621
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-05 03:34:32 -0700

    manly man

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,11 @@
 *
 **Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int n;
 int x;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 x = n % 10;
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,12 @@
 int main(void)
 {
 int ch;
+
 for (ch = 97; ch <= 122; ch++)
 {
 putchar(ch);
 }
 putchar(10);
+
 return (0);
 }
