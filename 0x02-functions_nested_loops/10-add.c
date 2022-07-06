214935e29df58693006ac6b581e978b5daea1503
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-06 14:55:08 -0700

    am still here betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,34 +1,14 @@
 #include "main.h"
 
 /**
-*times_table -> time table
+*add -> adds two number
+* @x : param1
+* @y : param2
+*
+*Return: x + y
 */
 
-void times_table(void)
-
+int add(int x, int y)
 {
-int x, y, k;
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y < 10; y++)
-{
-k = x * y;
-if (y == 0)
-_putchar(k + '0');
-if (y != 0 && k < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
+return (x + y);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-
-
 /**
 *times_table -> time table
+*
 */
 
 void times_table(void)
@@ -25,6 +24,7 @@ _putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10)
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
