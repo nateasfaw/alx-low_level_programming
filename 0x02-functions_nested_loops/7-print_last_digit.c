e3044dc9a8885e77b2b69ce3fb0082ad6df863d7
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-06 14:26:53 -0700

    am still here betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,40 +1,20 @@
 #include "main.h"
 
 /**
-* jack_bauer -> prints 24 hours
-*
+*print_last_digit - prints the last digit of a number
+*@n: the int to extract the last digit from
+*Return: value of the last digit
 */
 
-void jack_bauer(void)
+int print_last_digit(int n)
 
 {
-int i, j;
-for (i = 0; i < 24; i++)
-{						
-	for (j = 0; j < 60; j++)
-{
-if (i < 10)
-{
-_putchar('0');
-_putchar(i + '0');
-else if (i >= 10)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');	
-}
-	if (j < 10)
-{
-_putchar(':');
-_putchar('0');
-_putchar(j + '0');
-}
-else if (j >= 10)
-{	
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
-}
+int a;
+if (n < 0)
+n = -n;
+a = n % 10;
+if (a < 0)
+a = -a;
+_putchar(a + '0');
+return (a);
 }
