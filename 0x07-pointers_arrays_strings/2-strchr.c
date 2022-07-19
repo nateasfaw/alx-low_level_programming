7dd73660d1ebaf9944b1fca56ad2773cae25720c
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-19 02:02:43 -0700

    smile

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
 /**
-*_strchr -> string character
+*_strchr-> string character
 *@s: string given
 *@c: another char
 *Return: a string
 */
-
 char *_strchr(char *s, char c)
 {
-int a;
-while (1)
+int a = 0, b;
+while (s[a])
+a++;
+for (b = 0; b < a; b++)
 {
-a = *s++;
-if (a == c)
-{
-return (s - 1);
+(if c - s[b])
+	s += b;
+return (s);
 }
-if (a == 0)
-{
-return (NULL);
-}
-}
+return ('/0');
 }
