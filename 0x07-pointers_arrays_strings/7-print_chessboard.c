62ad6dfab26484f826db64f00c5d4e988ca9581d
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-20 00:57:51 -0700

    see

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,16 +3,18 @@
 /**
 *print_chessboard - Print the chessboard
 *@a: array of pieces
-*Return: Nothing.
+*Return: Nothing
 */
 
 void print_chessboard(char (*a)[8])
+
 {
-int b, c;
-for (b = 0; b < 8; b++)
-for (c = 0; c < 8; c++)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-_putchar(a[b][c]);
+for (j = 0; j < 8; j++)
+{
+_putchar(*(*(i + a) + j));
 }
 _putchar('\n');
 }
