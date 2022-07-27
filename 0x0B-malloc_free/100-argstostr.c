fcd49c44ab85acda2b97c0edb9f1a5d620aa923f
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-27 12:54:37 -0700

    faint

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*argstostr ->function that concatenates all the arguments of your program
+*@ac: argument count
+*@av: array of pointers to arguments
+*Return: arguments with new line or NULL in error
+*/
+
+char *argstostr(int ac, char **av)
+{
+int agc, byte, size = ac, index;
+char *agv;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (agc = 0; agc < ac; agc++)
+{
+for (byte = 0; av[agc][byte]; byte++)
+size++;
+}
+agv = malloc(sizeof(char) * size + 1);
+if (agv == NULL)
+{
+return (NULL);
+}
+index = 0;
+for (agc = 0; agc < ac; agc++)
+for (byte = 0; av[agc][byte]; byte++
+{
+agv[index++] = av[agc][byte];
+}
+agv[index++] = '\n';
+}
+agv[size] = '\0';
+return (agv);
+}
