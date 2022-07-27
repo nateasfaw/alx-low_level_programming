ba72027da8edf343d958ffe836ca28b856b405ef
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-07-27 01:24:33 -0700

    please work

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,7 +11,7 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
