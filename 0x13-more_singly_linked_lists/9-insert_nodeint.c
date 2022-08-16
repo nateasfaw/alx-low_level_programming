1d349ae0a7765321163050440e4577f5b9c3c2d4
Natnael Asfaw Atnafu <nateasfaw@github.com>
2022-08-16 06:08:14 -0700

    massive

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
 *@n: The data to be added
 *Return: the address of the new node or NULL if it failed
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t**head, unsigned int idx, int n)
 {
 unsigned int count = 0;
 listint_t *temp, *new_node;
